9905e7305485e1f21b9ee0d8b12b4f54b1de68fd
Daniel Pono Takamori <dtakamori@contractor.linuxfoundation.org>
2019-04-05 11:43:17 -0700

    start beaver barcamp2019 slides

---
+++ 2019/barcamp.rkt
@@ -0,0 +1,27 @@
+#lang slideshow
+
+(begin (current-main-font "Fira Sans")
+       (current-font-size 70)
+       (slide (t "Sustainability")
+              (text "Do it for Her"
+                    "Fira Sans, ExtraLight"
+                    (current-font-size))))
+
+(begin (current-font-size 45)
+    (slide
+     (t "Daniel Pono Takamori")
+     'next
+     (item (text "Cloud Custodian for Hire"
+                    "Fira Sans, ExtraLight"
+                    (current-font-size)))
+     'next
+     (item (text "Semi-pro Starcraft 2 and amateur go"
+                    "Fira Sans, ExtraLight"
+                    (current-font-size)))
+     'next
+     (item (text "emptytriangle on Instagram"
+                    "Fira Sans, ExtraLight"
+                    (current-font-size))))
+
+    (slide
+     (t "games describe an experience - someone very smart")))
